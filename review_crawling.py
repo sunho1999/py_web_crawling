7477692f36d07ce7c2af7548af34b87ea06b6dd1
sunho1999 <wmfrlek11@naver.com>
2021-06-28 21:55:56 +0900

    다양한 리뷰 모아서 시각화하기 미완성상태

--- review_crawling.py
+++ review_crawling.py
@@ -1,5 +1,10 @@
 import urllib.request
 from bs4 import BeautifulSoup
+import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
+
+
 
 
 class Review:
@@ -40,3 +45,54 @@ title, review_list = crawl("https://movie.naver.com/movie/bi/mi/basic.nhn?code=3
 print('제목: ' + title)
 for review in review_list:
     review.show()
+def get_summary(review_list):
+    star_list = []
+    good_list = []
+    bad_list = []
+    for review in review_list:
+        star_list.append(int(review.star))
+        good_list.append(int(review.good))
+        bad_list.append(int(review.bad))
+
+
+    star_series = pd.Series(star_list)
+    good_series = pd.Series(good_list)
+    bad_series = pd.Series(bad_list)
+    summary = pd.DataFrame({
+        'Star': star_series,
+        'Good': good_series,
+        'Bad': bad_series,
+        'Score': good_series / (good_series + bad_series)
+    })
+    return summary
+
+
+movie_code_list = [136900, 167657, 174321, 184859, 167391]
+
+
+review_lists = []
+for i in movie_code_list:
+    title, review_list = crawl("https://movie.naver.com/movie/bi/mi/basic.nhn?code=" + str(i))
+    summary = get_summary(review_list)
+    print("[ %s ]" % (title))
+    print(summary)
+    review_lists.append((title, review_list))
+
+
+def movie_compare(review_lists):
+    count = 1
+    x = []
+    y = []
+    for movie, review_list in review_lists:
+        x.append(count)
+        summary = get_summary(review_list)
+        summary = summary[summary['Score'] > 0.8]
+        y.append(summary['Star'].mean())
+        count += 1
+    plt.bar(x, y)
+    plt.title('영화 별점 비교')
+    plt.xlabel('영화 번호')
+    plt.ylabel('신뢰성 별점 평균')
+    plt.show()
+movie_compare(review_lists)
+
